af5a9efcb47df4641f82ff147a919ebe9866f63a
Komal <waghmarekomal21@gmail.com>
2022-09-08 00:30:57 +0530

    [Komal] Create Contact List for Address Book

---
+++ AddressBook/CreateContact.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook
+{
+    internal class CreateContact
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public double Zip { get; set; }
+        public double PhoneNumber { get; set; }
+        public string EmailId { get; set; }
+    }
+}
